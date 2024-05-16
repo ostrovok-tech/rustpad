30d1dd204824695462105790f07a7682b4911570
Ivan Elfimov <ielfimov@gmail.com>
2024-05-16 10:10:53 +0300

    Make sure no external deps are downloaded from other sites

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,11 @@ import init, { set_panic_hook } from "rustpad-wasm";
 import App from "./App";
 import "./index.css";
 
+// This is important to avoid requests to jsDelivr and other external CDNs
+import { loader } from "@monaco-editor/react";
+import * as monaco from "monaco-editor";
+loader.config({ monaco });
+
 init().then(() => {
   set_panic_hook();
   ReactDOM.render(
@@ -13,6 +18,6 @@ init().then(() => {
         <App />
       </ChakraProvider>
     </StrictMode>,
-    document.getElementById("root")
+    document.getElementById("root"),
   );
 });

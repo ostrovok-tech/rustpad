b5e76fcade2c19322e1246350ea54563d3e9f598
Ivan Elfimov <ielfimov@gmail.com>
2024-05-29 15:29:06 +0300

    Upgrade deps, fix React root warning, fix workers loading

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import init, { set_panic_hook } from "rustpad-wasm";
 import App from "./App";
@@ -8,16 +8,33 @@ import "./index.css";
 // This is important to avoid requests to jsDelivr and other external CDNs
 import { loader } from "@monaco-editor/react";
 import * as monaco from "monaco-editor";
+import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
+import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
+
+self.MonacoEnvironment = {
+  getWorker(_, label) {
+    if (label === "typescript" || label === "javascript") {
+      return new tsWorker();
+    }
+    return new editorWorker();
+  },
+};
+
 loader.config({ monaco });
+loader.init().then((monaco) => {
+  console.log("Monaco initialized");
+});
+
+const container = document.getElementById("root");
+const root = createRoot(container);
 
 init().then(() => {
   set_panic_hook();
-  ReactDOM.render(
+  root.render(
     <StrictMode>
       <ChakraProvider>
         <App />
       </ChakraProvider>
     </StrictMode>,
-    document.getElementById("root"),
   );
 });
